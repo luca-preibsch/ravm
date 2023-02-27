143ca39faa36d79ae00b597821c331edd3f2d41f
Luca Preibsch <luca.preibsch@fau.de>
2023-02-27 21:10:50 +0100

    add dialogue popup with dummy content

--- src/background/ui/trust-dialogue/trust-dialogue.js
+++ src/background/ui/trust-dialogue/trust-dialogue.js
@@ -1,10 +1,11 @@
 import './style.css'
 import '../../../style/button.css'
+import './trust-dialogue.html'
 
 export function showDialogue() {
     let createData = {
         type: "detached_panel",
-        url: "./index.html",
+        url: "./trust-dialogue.html",
         width: 250,
         height: 100
       }
