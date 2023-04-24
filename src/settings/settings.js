f9c4f55b247653c652d9ff8a3fc9972a09c5383c
Luca Preibsch <luca.preibsch@fau.de>
2023-04-24 20:21:38 +0200

    add button to settings that shows more information about the host (currently shows dummy data and needs styling)

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -2,21 +2,23 @@ import './style.css'
 import '../style/table.css'
 
 import * as storage from '../lib/storage'
+import {AttesationReport} from "../lib/attestation";
 
-const table = document.getElementById("tableBody")
-const form = document.getElementById("form")
-
-// TODO: button for showing the measurement
-
-let counter = 0;
-document.getElementById("testButton").onclick = async function () { await saveItem(counter++ + "example.com", new Date(), new Date(), "AMD-SEV", "xxx") }
+const table = document.getElementById("tableBody");
+const submitButton = document.getElementById("submitButton");
+const modal = document.querySelector(".modal");
 
 function onRemove() {
-    const toRemove = [...form.querySelectorAll(".removeCheckbox")].filter(el => el.checked);
+    const toRemove = [...table.querySelectorAll(".removeCheckbox")].filter(el => el.checked);
     toRemove.forEach(el => storage.removeHost(el.value));
 }
 
-form.addEventListener("submit", onRemove);
+submitButton.addEventListener("click", onRemove);
+
+modal.addEventListener("click", (e) => {
+    if (e.target.id !== "modalContent")
+        modal.close();
+});
 
 function loadAllItems() {
     table.innerHTML = ""
@@ -28,7 +30,7 @@ function loadAllItems() {
                 row.insertCell().appendChild(createTitleCell(host));
                 row.insertCell().innerHTML = data.trustedSince.toLocaleString();
                 row.insertCell().innerHTML = data.lastTrusted.toLocaleString();
-                row.insertCell().innerHTML = data.type;
+                row.insertCell().appendChild(createButton(data));
             }
         },
         (reason) => {
@@ -37,12 +39,17 @@ function loadAllItems() {
 }
 
 window.addEventListener("load", loadAllItems);
-browser.storage.onChanged.addListener(loadAllItems)
+browser.storage.onChanged.addListener(loadAllItems);
 
 async function saveItem(domain, trustedSince, lastTrusted, type, ar_arrayBuffer) {
     await storage.newTrusted(domain, trustedSince, lastTrusted, type, ar_arrayBuffer)
     console.log("save")
-    loadAllItems()
+}
+
+function showModal(hostInfo) {
+    const ar = new AttesationReport(hostInfo.ar_arrayBuffer);
+    modal.querySelector("#modalContent").innerText = ar.parse_report;
+    modal.showModal();
 }
 
 function createTitleCell(title) {
@@ -59,4 +66,12 @@ function createTitleCell(title) {
     div.appendChild(checkbox);
     div.appendChild(label);
     return div;
+}
+
+function createButton(hostInfo) {
+    const button = document.createElement("button");
+    button.classList.add("link");
+    button.innerText = "more info";
+    button.onclick = function () {showModal(hostInfo)};
+    return button;
 }
