b41af967818eba8f5a49894d8342fe18021e9fa1
Luca Preibsch <luca.preibsch@fau.de>
2023-09-12 15:10:26 +0200

    cleanup benchmark script

--- evaluation/selenium/benchmark.py
+++ evaluation/selenium/benchmark.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 import time
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,25 +8,25 @@ from selenium.webdriver.support import expected_conditions as EC
 erweiterung_pfad = '/Users/luca/Library/CloudStorage/OneDrive-Persönlich/Dokumente/Uni/BA/Builds/17840039dd4943e1851d-1.1.2.xpi'
 # firefox_profile_path = '../../firefox-profile'
 firefox_profile_path = './firefox-profile'
-
-# Definiere Optionen, um den Cache zu deaktivieren
-# options = Options()
-options = webdriver.FirefoxOptions()
-options.set_preference("browser.cache.disk.enable", False)
-options.set_preference("browser.cache.memory.enable", False)
-options.set_preference("browser.cache.offline.enable", False)
-options.set_preference("network.http.use-cache", False)
-# options.set_preference("profile", firefox_profile_path)
-options.profile = firefox_profile_path
-
-# Erstelle den WebDriver mit den angegebenen Optionen und Profil
-driver = webdriver.Firefox(options=options)
-
 # URL der zu testenden Webseite
 url = 'https://transparent-vm.net:8080'
 
+# options = Options()
+options = webdriver.FirefoxOptions()
+
+# Definiere Optionen, um den Cache zu deaktivieren
+# Nicht mehr nötig, da Browser für jeden Test neu gestartet wird
+# options.set_preference("browser.cache.disk.enable", False)
+# options.set_preference("browser.cache.memory.enable", False)
+# options.set_preference("browser.cache.offline.enable", False)
+# options.set_preference("network.http.use-cache", False)
+
+# options.set_preference("profile", firefox_profile_path)
+options.profile = firefox_profile_path
+# options.add_argument("-profile firefox_profile_path")
+
 # Anzahl der Wiederholungen
-anzahl_wiederholungen = 10
+anzahl_wiederholungen = 3
 
 # Liste zur Speicherung der Ladezeiten
 ladezeiten = []
@@ -41,18 +38,16 @@ for _ in range(anzahl_wiederholungen):
     # Lade die Erweiterung
     driver.install_addon(erweiterung_pfad)
 
-    time.sleep(2)
+    # be sure the browser was already open before starting the measurement
+    driver.get("https://example.com")
 
     # Navigiere zur Webseite
     startzeit = time.time()  # Startzeit messen
     driver.get(url)
-    print(driver.current_url)
 
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, timeout=10, poll_frequency=1/1000).until(
         EC.presence_of_element_located((By.ID, 'test123'))
     )
-
-    print(driver.current_url)
 
     endzeit = time.time()  # Endzeit messen
     ladezeit = (endzeit - startzeit) * 1000  # Ladezeit berechnen
@@ -60,14 +55,14 @@ for _ in range(anzahl_wiederholungen):
 
     # Lösche Cookies und Local Storage
     driver.quit()
+
+    # Nicht mehr nötig, da Browser neu gestartet wird
     # driver.delete_all_cookies()
     # driver.execute_script('window.localStorage.clear();')
 
 # Berechne Durchschnittliche Ladezeit
 durchschnittliche_ladezeit = sum(ladezeiten) / len(ladezeiten)
 
-# Schließe den Browser
-# driver.quit()
-
 # Gib die Ergebnisse aus
 print(f'Durchschnittliche Ladezeit nach {anzahl_wiederholungen} Versuchen: {durchschnittliche_ladezeit} ms')
+print(f'Lowest: {min(ladezeiten)}, highest: {max(ladezeiten)}')
